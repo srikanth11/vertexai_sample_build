21d8647ebbd00865a9615ba5c2a89d5f1b1bbf36
srikanth <srikanth6835@gmail.com>
2023-12-07 18:10:04 +0530

    added simple python execute through cloud build ymal file

---
+++ sample.py
@@ -0,0 +1,8 @@
+# sample function to execute it by sample trigger.
+
+def sample_function():
+    print("Its a test function123")
+    return "True"
+
+if __name__ == "__main__":
+    status_check = sample_function()
